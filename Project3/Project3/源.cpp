126a8e3cd7e954d4e4aa19c332cb9e9e5e34eb50
Eric-bit <376695558@qq.com>
2020-05-18 22:40:09 +0800

    code

---
+++ Project3/Project3/源.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//long long int Fib(int N)
+//{
+//	if (N < 3)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return Fib(N - 1) + Fib(N - 2);
+//	}
+//}
+//int main()
+//{
+//	int N;
+//	scanf("%d", &N);
+//	long long int sum;
+//	sum = Fib(N);
+//	printf("%d", sum);
+//}
+//将两个有序数列合并
+#include<stdio.h>
+int main()
+{
+	int arr1[4] = { 2, 4, 6, 8 };
+	int arr2[4] = { 1, 3, 5, 7 };
+	
+}
