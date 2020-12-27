6b4b48a12fee5f8ed4b88b71cfde7f0b38319c87
Old-Weirdo <376695558@qq.com>
2020-12-27 22:44:47 +0800

    code

---
+++ test_12_26/test_12_26/源.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+using namespace std;
+void main(){
+	string a;
+	string b;
+	tolower()
+	cin >> a;
+	cin >> b;
+	if (a == b){
+		cout << "YES" << endl;
+	}
+	else
+	{
+		cout << "NO" << endl;
+	}
+}
